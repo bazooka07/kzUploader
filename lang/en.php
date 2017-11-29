bf2bc9cf8073261e9d281a06ee083dc7359858d3
bazooka07 <jpourrez@gmail.com>
2017-11-29 18:58:24 +0100

    Corrections linuistiques mineures

--- lang/en.php
+++ lang/en.php
@@ -1,24 +1,24 @@
 <?php
 $LANG = array(
-	'L_BAD_TYPE_FILE'			=> 'Bad type of file',
+	'L_BAD_TYPE_FILE'		=> 'Bad type of file',
 	'L_DOWNLOADED_PLUGINS'		=> 'Add %d new plugin(s)',
 	'L_INVALIDATE_PLUGIN'		=> 'Invalidate Class for the plugin',
 	'L_INVALIDATE_THEMA'		=> 'Invalidate theme',
 	'L_MISSING_INFOS_FILE'		=> 'The file infos.xml is missing',
 	'L_MISSING_LIBRARIES'		=> 'Missing ZipArchive and Phar libraries',
-	'L_NEW_PLUGINS'				=> '%d new plugin(s)',
-	'L_NEW_THEMAS'				=> '%d new theme(s)',
+	'L_NEW_PLUGINS'			=> 'New plugin(s)',
+	'L_NEW_THEMAS'			=> 'New theme(s)',
 	'L_NOT_JUST_ONE_FOLDER'		=> 'Invalidate archive. Must contain only one folder',
 	'L_PLUGIN_ALREADY_EXISTS'	=> 'A plugin with the same name already exists',
-	'L_POST_MAX_SIZE'			=> 'The size of the upload is too big (maxi: %d Ko)',
+	'L_POST_MAX_SIZE'		=> 'The size of the upload is too big (maxi: %d Ko)',
 	'L_THEMA_ALREADY_EXISTS'	=> 'A theme with the same name already exists',
-	'L_UNWRITABLE_FOLDER'		=> 'The folder %s is unwritable.'
-	'L_UPLOAD'					=> 'Upload',
+	'L_UNWRITABLE_FOLDER'		=> 'The folder %s is unwritable.',
+	'L_UPLOAD'			=> 'Upload',
 
 	/*  Pour Javascript */
-	'JS_LIMITS_UPLOAD'			=> 'One ou several limits are trepassed',
+	'JS_LIMITS_UPLOAD'		=> 'One ou several limits are trepassed',
 	'JS_MAX_FILE_UPLOADS'		=> '##1## files ( max: ##2## )',
-	'JS_POST_MAX_SIZE'			=> 'Sum of ##1## Ko ( max: ##2## )',
+	'JS_POST_MAX_SIZE'		=> 'Sum of ##1## Ko ( max: ##2## )',
 	'JS_UPLOAD_MAX_FILESIZE'	=> '##1## big file(s) ( max: ##2## Ko )'
 );
 ?>
